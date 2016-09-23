547ef751f5afc70631b0be636b41c581a8e54298
Alois Barreras <alois@vicino.io>
2016-09-23 19:29:08 -0400

    initial commit

---
+++ test/index.js
@@ -0,0 +1,36 @@
+var Test = require('segmentio-integration-tester');
+var assert = require('assert');
+var AmazonKinesis = require('../lib');
+
+describe('Amazon Kinesis', function() {
+    var kinesis;
+    var settings;
+    var test;
+
+    beforeEach(function(){
+        settings = {
+        };
+        kinesis = new AmazonKinesis(settings);
+        test = Test(kinesis, __dirname);
+    });
+
+    it('should have correct settings', function(){
+        test
+        .name('Amazon Kinesis')
+        .channels(['server'])
+        .ensure('settings.streamName');
+    });
+
+    describe('.track()', function(){
+        it('should send track correctly', function(done){
+            var json = test.fixture('track-basic');
+            test
+            .set(settings)
+            .track(json.input)
+            .end(function(err, res){
+                if (err) return done(err);
+                done();
+            });
+        });
+    });
+});
